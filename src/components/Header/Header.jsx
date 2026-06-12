dd74776645f5e91df453fb0a587c9d39302568e4
Muhammad Zohaib <zohaibsoomro100@gmail.com>
2026-06-12 22:05:27 +0500

    herosection work done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import logoImage from '../../assets/logo.png'
+import './header.css'
 
 const Header = () => {
+
+
+  let headerMenu = [
+    {
+      menuItemName: "Home",
+      link: "/home"
+    },
+    {
+      menuItemName: "About Me",
+      link: "/aboutme"
+    },
+    {
+      menuItemName: "Projects",
+      link: "/projects"
+    },
+    {
+      menuItemName: "Certificates",
+      link: "/certificates"
+    },
+    {
+      menuItemName: "Contact Me",
+      link: "/contact"
+    },
+  ]
+
+
+
   return (
-    <div>Header</div>
+
+    <header className='header'>
+      <img src={logoImage} alt="logo" />
+
+
+      <ul className='menuItems'>
+        {headerMenu.map((item) => (
+          <li>{item.menuItemName}</li>
+        ))}
+      </ul>
+
+    </header>
   )
 }
 
--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
 
+import { FaGithub } from "react-icons/fa";
+import profilepicture from '../../assets/profilepicture.jpg'
+import { SiHtml5 } from "react-icons/si";
+import { FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { GrReactjs } from "react-icons/gr";
+
+import './heroSection.css'
+
+
+
 const HeroSection = () => {
+
+  let techIcons = [
+    <SiHtml5 />,
+    <FaCss3Alt />,
+    <IoLogoJavascript />,
+    <GrReactjs />
+  ]
+
+
   return (
-    <div>HeroSection</div>
+    <div className='heroSection'>
+      {/* FIRST CONTAINER */}
+      <div className='heroSection-firstContainer'>
+        <div className='heroSection-firstContainer-leftSection'>
+          <h1>Frontend ReactJS Developer</h1>
+
+
+          <p>Hi, I'm <span>Muhammad Hamza</span>. A passionate Full Stack NextJS | MERN Stack | ReactJS Developer based in Karachi, Pakistan. 📍</p>
+
+
+          <FaGithub className='icon'/>
+        </div>
+
+        {/* IMAGE CONTAINER */}
+        <div className='heroSection-image'>
+          <img src={profilepicture} alt="" />
+        </div>
+      </div>
+
+      {/* SECOND CONTAINER */}
+      <div>
+        <h3>Tech Stack</h3>
+
+        <span>|</span>
+
+      <div>
+        {techIcons.map((items) => (
+          items
+        ))}
+      </div>
+      </div>
+    </div>
   )
 }
 
