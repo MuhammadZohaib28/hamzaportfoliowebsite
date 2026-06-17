f4594515f502c8229f9dacfe2d756fbc14f12578
Muhammad Zohaib <zohaibsoomro100@gmail.com>
2026-06-17 22:11:22 +0500

    projects and aboutme

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import './aboutMe.css'
 
 const AboutMe = () => {
   return (
-    <div>AboutMe</div>
+    <div className='aboutme-section'>
+      <div className='aboutme-image-container'>
+        <img src="https://muhammadzuhaib.vercel.app/assets/aboutmee-7a57c3bb.svg" alt="" />
+      </div>
+
+
+      <div className='aboutme-right-section'>
+        <h1>About Me</h1>
+
+        <h2>
+          A dedicated Frontend Developer based in Karachi, Pakistan. 📍
+        </h2>
+
+
+        <p>
+          As a Junior Frontend Developer, I possess an impressive arsenal of skills in <span>HTML5</span> , <span>CSS3</span>, <span>JavaScript</span>, <span>ReactJS</span>. I excel in designing and maintaining <span>responsive websites</span> that offer a smooth user experience. My expertise lies in crafting <span>dynamic</span>, engaging interfaces through writing <span>clean</span> and <span>optimized code</span> and utilizing cutting-edge development tools and techniques. I am also a <span>team player</span> who thrives in collaborating with cross-functional teams to produce outstanding web applications.
+        </p>
+      </div>
+    </div>
   )
 }
 
--- src/components/ProjectSection/ProjectSection.jsx
+++ src/components/ProjectSection/ProjectSection.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import './projectSection.css'
+
+import projectpicture from '../../assets/image.png'
 
 const ProjectSection = () => {
+
+  let projectsData = [
+    {
+      projectImage: projectpicture,
+      projectName: "Portfolio Webiste 😍",
+      projectDescription: "This is a Course project. I have worked on this website using ReactJS.",
+      projectTagOne: "ReactJs",
+      projectTagTwo: "CSS3"
+
+    },
+    
+
+  ]
+
   return (
-    <div>ProjectSection</div>
+    <div className='projects-container'>
+      <h1>Projects</h1>
+
+      <h2>Each project is a unique piece of development 🧩</h2>
+
+      <div className='projects-single-container'>
+
+        {projectsData.map((item) => (
+          <>
+            <div className='projects-single-image-container'>
+              <img src={item.projectImage} alt="" />
+            </div>
+
+
+            <div className='projects-details'>
+
+
+              <h2>{item.projectName}</h2>
+
+
+              <p>{item.projectDescription}</p>
+
+              <div className='project-section-tags'>
+                <span>{item.projectTagOne}</span>
+                <span>{item.projectTagTwo}</span>
+              </div>
+            </div>
+          </>
+        ))}
+
+      </div>
+    </div>
   )
 }
 
